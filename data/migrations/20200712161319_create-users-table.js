cd0987e33379b45d03d15dcd989178d309a0293d
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-12 16:17:05 -0400

    Created migration file for users table

---
+++ data/migrations/20200712161319_create-users-table.js
@@ -0,0 +1,12 @@
+
+exports.up = async function(knex) {
+	await knex.schema.createTable('users', tbl => {
+		tbl.increments();
+		tbl.text('username').notNullable().unique();
+		tbl.text('password').notNullable();
+	})
+}
+
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists('users');
+}
