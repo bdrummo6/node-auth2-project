124b82f682912e39a3c45af9501ffc5f9bf9c5e2
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-12 20:27:38 -0400

    Create migration

---
+++ database/migrations/20200712202354_create-users-table.js
@@ -0,0 +1,14 @@
+
+
+exports.up = async function(knex) {
+	await knex.schema.createTable('users', tbl => {
+		tbl.increments();
+		tbl.text('username').notNullable().unique();
+		tbl.text('password').notNullable();
+		tbl.string('department');
+	})
+}
+
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists('users');
+}
