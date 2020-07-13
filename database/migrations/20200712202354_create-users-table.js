af9cf2870d87d6088ba61c8be9a2b27d5ec64625
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-13 15:20:37 -0400

    Fixed JSON
    changer server script so that login endpoint works.

--- database/migrations/20200712202354_create-users-table.js
+++ database/migrations/20200712202354_create-users-table.js
@@ -3,9 +3,9 @@
 exports.up = async function(knex) {
 	await knex.schema.createTable('users', tbl => {
 		tbl.increments();
-		tbl.string('username', 128).notNullable().unique();
-		tbl.string('password', 256).notNullable();
-		tbl.string('department', 128).notNullable();
+		tbl.text('username').notNullable().unique();
+		tbl.text('password').notNullable();
+		tbl.text('department').notNullable();
 	})
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const session = require('express-session'); // Imported for dealing with login sessions
-const KnexSessionStore = require('connect-session-knex')(session); // Imported for creating login session tables to store sessions
-const db = require('./database/dbConfig');
+const cookieParser = require('cookie-parser');
 
 const welcomeRouter = require('./api/welcome/welcome-router');
 const usersRouter = require('./api/users/users-router');
@@ -14,32 +12,19 @@ const port = process.env.PORT || 8000;
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser());
 
-server.use(session({
-	resave: false, // avoids recreating sessions that have not changed
-	saveUninitialized: false,
-	secret: 'secrets are necessary',
-	store: new KnexSessionStore({
-		knex: db, // configured instance of Knex, or the live database connection
-		createtable: true, // if the session table does not exist, create it
-	}),
-}));
-
-server.use('/api',welcomeRouter);
-server.use('/api',usersRouter);
+server.use('/api', welcomeRouter);
+server.use('/api', usersRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err);
+	console.log(err)
 
 	res.status(500).json({
-		message: 'Something went wrong!',
+		message: 'Something went wrong',
 	});
 })
 
-server.get('/', (req, res) => {
-	res.send(`The server is running at http://localhost:${port}`);
-})
-
 server.listen(port, () => {
-	console.log(`Running at http://localhost:${port}`);
+	console.log(`Running at http://localhost:${port}`)
 })
